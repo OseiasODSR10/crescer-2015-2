4d53fb66c3015dccf20e82e9aa6a0de77bd16256
Oséias Rodrigues <oseias.rdgs@hotmail.com>
2015-09-16 17:13:40 -0300

    no message

--- src/modulo-01-java-OO/Modulo01/Dwarf.java
+++ src/modulo-01-java-OO/Modulo01/Dwarf.java
@@ -2,16 +2,12 @@
 public class Dwarf
 {
     private int vida;
-    private String nome;
     
-    public Dwarf (String nome){
-        this.nome = nome;
+    public Dwarf (){
         this.vida = 110;
     }
     
     public void perderVida(){
-        if(this.vida >=10){
             this.vida -= 10;
-        }
     }
 }
