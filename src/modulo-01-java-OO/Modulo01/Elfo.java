872ed7ba87da83aa38b4e2ffd27c5e1ca2b66575
OseiasODSR10 <oseias.rdgs@hotmail.com>
2015-09-16 12:19:59 -0300

    Update Elfo.java

--- src/modulo-01-java-OO/Modulo01/Elfo.java
+++ src/modulo-01-java-OO/Modulo01/Elfo.java
@@ -1,9 +1,33 @@
 public class Elfo {
     private String nome;
     private int flechas;
+    private int experiencia;
     
     public Elfo(String n, int flech) {
         nome = n;
         flechas = flech;
+    }
+    
+    public Elfo(String n){
+    	nome = n;
+    	flechas = 42;
+    }
+   
+    public boolean acertar(){
+		int x = (int) Math.random() * 100; 
+		return x<50;
+	}
+
+	public void atirarFlecha(){
+		flechas--;
+		if(acertar() == true){
+			experiencia++;
+		}  
+	}
+    
+    public void mostrarInformações(){
+    	System.out.println("Elfo: "+nome);
+    	System.out.println("Experiência: "+experiencia);
+    	System.out.println("Flechas: "+flechas);
     }
 }
