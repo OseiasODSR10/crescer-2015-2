2125e7bf99bce618b1d4660fa13b0e61253a642d
Oséias Rodrigues <oseias.rdgs@hotmail.com>
2015-10-21 16:02:57 -0200

    exercicios lista 3 corrigidos e exercicios aula 4.

--- src/javascript/lista-js-3.js
+++ src/javascript/lista-js-3.js
@@ -1,3 +1,5 @@
+
+
 function Item (sku, descricao, valorUnitario){
   this.sku = sku;
   this.descricao = descricao;
@@ -9,9 +11,12 @@ Item.prototype.calcularSubTotal = function (){
   return this.valorUnitario*this.quantidade;
 };
 
+
+
+
 function CarrinhoDeCompras(){
-  this.itensNoCarrinho =
-  [];
+  this.itensNoCarrinho = [];
+  this.precoTotal = 0;
 };
 
 CarrinhoDeCompras.prototype.adicionarItemAoCarrinho = function(item){
@@ -23,16 +28,51 @@ CarrinhoDeCompras.prototype.removerItemDoCarrinhoPeloSKU = function(sku){
   });
 };
 CarrinhoDeCompras.prototype.atualizarQuantidade = function(sku, qtd){
-  var item = this.itensNoCarrinho.filter(function(elem){
+  for (var i=0;i<this.itensNoCarrinho.length;i++) {
+    var elemAtual = this.itensNoCarrinho[i];
+    if (elemAtual.sku === sku) {
+      elemAtual.quantidade = qtd;
+    }
+  }
+
+  /*var item = this.itensNoCarrinho.filter(function(elem){
     return elem.sku === sku;
   })[0];
   item.quantidade = qtd;
   this.removerItemDoCarrinhoPeloSKU(sku);
-  this.adicionarItemAoCarrinho(item);
+  this.adicionarItemAoCarrinho(item);*/
 };
 
 CarrinhoDeCompras.prototype.calcularTotal = function(){
-  return this.itensNoCarrinho.reduce(function (acumulador, elemAtual){
+  this.precoTotal = this.itensNoCarrinho.reduce(function (acumulador, elemAtual){
     return acumulador + elemAtual.calcularSubTotal();
   }, 0);
-}
+};
+
+CarrinhoDeCompras.prototype.sortearDesconto = function(){
+  var numeroSorte = Math.random();
+  if(numeroSorte < 0.4){
+    this.aplicarDesconto();
+  }
+};
+
+CarrinhoDeCompras.prototype.aplicarDesconto = function(){
+  this.precoTotal = (this.precoTotal*0.9);
+};
+
+CarrinhoDeCompras.prototype.forcarCompra = function() {
+  if (!this.intervalo) {
+    // var self = this;
+    this.intervalo = setInterval(function() {
+      this.itensNoCarrinho.forEach(function(elem) {
+        elem.valorUnitario += elem.valorUnitario * .1;
+      });
+    }.bind(this), 5000);
+  }
+};
+
+CarrinhoDeCompras.prototype.concluirPedido = function() {
+  clearInterval(this.intervalo);
+  // remover propriedade intervalo do objeto
+  delete this.intervalo;
+};
