6b4af08f15068dc4b6d142d58a74d3e0df7b6e7a
Oseias <oseias.rdgs@hotmail.com>
2015-10-20 23:44:04 -0200

    lista 3 exercicios 1-5.

--- src/javascript/lista-js-3.js
+++ src/javascript/lista-js-3.js
@@ -5,18 +5,34 @@ function Item (sku, descricao, valorUnitario){
   this.valorUnitario = valorUnitario;
 };
 
+Item.prototype.calcularSubTotal = function (){
+  return this.valorUnitario*this.quantidade;
+};
+
 function CarrinhoDeCompras(){
   this.itensNoCarrinho =
   [];
-}
+};
 
 CarrinhoDeCompras.prototype.adicionarItemAoCarrinho = function(item){
   this.itensNoCarrinho.push(item);
 };
 CarrinhoDeCompras.prototype.removerItemDoCarrinhoPeloSKU = function(sku){
-  this.itensNoCarrinho.forEach(function (elem){
-    if(elem.sku === sku){
-      this.itensNoCarrinho.splice(this.itensNoCarrinho.indexOf(elem),1);
-    }
-  })
+  this.itensNoCarrinho = this.itensNoCarrinho.filter(function(elem) {
+    return elem.sku !== sku;
+  });
+};
+CarrinhoDeCompras.prototype.atualizarQuantidade = function(sku, qtd){
+  var item = this.itensNoCarrinho.filter(function(elem){
+    return elem.sku === sku;
+  })[0];
+  item.quantidade = qtd;
+  this.removerItemDoCarrinhoPeloSKU(sku);
+  this.adicionarItemAoCarrinho(item);
+};
+
+CarrinhoDeCompras.prototype.calcularTotal = function(){
+  return this.itensNoCarrinho.reduce(function (acumulador, elemAtual){
+    return acumulador + elemAtual.calcularSubTotal();
+  }, 0);
 }
