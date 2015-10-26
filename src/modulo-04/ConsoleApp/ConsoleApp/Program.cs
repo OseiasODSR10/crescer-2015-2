2fb8784076b76d8f888b68f39e32ab4f7ad2bb42
Oséias Rodrigues <oseias.rdgs@hotmail.com>
2015-10-26 15:52:35 -0200

    aula 1 modulo 04

---
+++ src/modulo-04/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var contato1 = new Contato()
+            {
+                Nome = "Oseias",
+                Numero = 23456433
+            };
+
+            var contato2 = new Contato()
+            {
+                Nome = "Ben Hur",
+                Numero = 666
+            };
+            var contato3 = new Contato()
+            {
+                Nome = "Ben Hur",
+                Numero = 666
+            };
+
+            var agenda = new Agenda();
+            agenda.AdicionarContato(contato1);
+            agenda.AdicionarContato(contato2);
+            agenda.AdicionarContato(contato3);
+
+            agenda.ListarContatos();
+
+            System.Console.WriteLine("Após a remoção: ");
+
+            agenda.RemoverContato("Ben Hur");
+
+            agenda.ListarContatos();
+
+        }
+    }
+}
---
+++ src/modulo-04/ConsoleApp/UnitTestProject1/AgendaTeste.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp;
+
+namespace ConsoleAppTeste
+{
+    [TestClass]
+    public class AgendaTeste
+    {
+        [TestMethod]
+        public void agendaTemUmContato()
+        {
+            var agenda = new Agenda();
+            agenda.AdicionarContato(new Contato() { Nome = "Julio César", Numero = 97879867 });
+            agenda.AdicionarContato(new Contato() { Nome = "Oseias", Numero = 08907896});
+            Assert.AreEqual(agenda.QuantidadeContatos, 2);
+        }
+    }
+}
