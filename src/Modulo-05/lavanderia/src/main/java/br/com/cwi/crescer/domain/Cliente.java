173a57910f76b165e51fe23a37e8cb6f2231a9fc
Oseias <oseias.rdgs@hotmail.com>
2015-11-22 22:11:51 -0200

    Mapear classes do projeto lavanderia

---
+++ src/Modulo-05/lavanderia/src/main/java/br/com/cwi/crescer/domain/Cliente.java
@@ -0,0 +1,123 @@
+package br.com.cwi.crescer.domain;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "Cliente")
+@SequenceGenerator(name = Cliente.SEQUENCE_NAME,
+					sequenceName = Cliente.SEQUENCE_NAME)
+public class Cliente {
+	
+	public static final String SEQUENCE_NAME = "SEQ_Cliente";
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = SEQUENCE_NAME)
+	@Column(name = "IDCliente")
+	private Long idCliente;
+	
+	@Column(name = "Nome", length = 70)
+	@Basic(optional = false)
+	private String nome;
+	
+	@Column(name = "CPF", length = 11)
+	@Basic(optional = false)
+	private String cpf;
+	
+	@Column(name = "Email", length = 100)
+	private String email;
+	
+	@Column(name = "Endereco", length = 50)
+	private String endereco;
+	
+	@Column(name = "Bairro", length = 50)
+	private String bairro;
+	
+	@Column(name = "IDCidade")
+	private Long idCidade;
+	
+	@Column(name = "CEP")
+	private Integer cep;
+	
+	@Column(name = "Situacao")
+	private Character situacao;
+
+	public Long getIdCliente() {
+		return idCliente;
+	}
+
+	public void setIdCliente(Long idCliente) {
+		this.idCliente = idCliente;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public Long getIdCidade() {
+		return idCidade;
+	}
+
+	public void setIdCidade(Long idCidade) {
+		this.idCidade = idCidade;
+	}
+
+	public Integer getCep() {
+		return cep;
+	}
+
+	public void setCep(Integer cep) {
+		this.cep = cep;
+	}
+
+	public Character getSituacao() {
+		return situacao;
+	}
+
+	public void setSituacao(Character situacao) {
+		this.situacao = situacao;
+	}
+}
---
+++ src/Modulo-05/lavanderia/src/main/java/br/com/cwi/crescer/domain/Servico.java
@@ -0,0 +1,45 @@
+package br.com.cwi.crescer.domain;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Servico")
+@SequenceGenerator(name = Servico.SEQUENC_NAME,
+					sequenceName = Servico.SEQUENC_NAME)
+public class Servico {
+	
+	public static final String SEQUENC_NAME = "SEQ_Servico";
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = SEQUENC_NAME)
+	@Column(name = "IDServico")
+	private Long idServico;
+	
+	@Column(name = "Descricao", length = 30)
+	@Basic(optional = false)
+	private String descricao;
+
+	public Long getIdServico() {
+		return idServico;
+	}
+
+	public void setIdServico(Long idServico) {
+		this.idServico = idServico;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
